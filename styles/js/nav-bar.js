3001db3e91f4e878ef4121106aadb275f808a90e
camora-sudo <clarisse.a.nazario@gmail.com>
2026-06-17 23:17:31 +0800

    ADDED: Inspector homepage, fixed navbar.js

--- styles/js/nav-bar.js
+++ styles/js/nav-bar.js
@@ -4,30 +4,41 @@ function showNavBar(){
     const page = path.split("/").pop().replace(".html", "");  
 
     let navItems = "";
+    let basePath = "";
 
     if(path.includes("/admin/")){
+        basePath = "../../";
         navItems = `
             <li class="nav-item"><a class="nav-link" href="../admin/dashboard.html">Dashboard</a></li>
         `;
     }
     else if(path.includes("/inspector/")){
+        basePath = "../../";
         navItems = `
             <li class="nav-item"><a class="nav-link" href="../inspector/dashboard.html">Dashboard</a></li>
             <li class="nav-item"><a class="nav-link" href="../inspector/inspection-entry.html">Log Entry</a></li>
-            <li class="nav-item"><a class="nav-link" href="../logout.html">Logout</a></li>
+            <li class="nav-item"><a class="nav-link" href="../login.html">Logout</a></li>
         `
     }
     else if(path.includes("/public/")){
+        basePath = "../../";
         navItems = `
             <li class="nav-item"><a class="nav-link" href="../public/complaint.html">Report</a></li>
             <li class="nav-item"><a class="nav-link" href="../login.html">Login</a></li>
         `;
     }
+    else if (path.includes("login")){
+        basePath = "../";
+        navItems = `
+            <li class="nav-item"><a class="nav-link" href="../login.html">Login</a></li>
+        `;
+    }
+
 
     const navBarHTML = `
         <nav class="navbar navbar-expand-lg navbar-dark navbar-foodsafe-custom">
             <div class="container-fluid">
-                <img src="../../src/images/logo.png" width="30" height="30" class="d-inline-block align-top mr-2" alt="">
+                <img src="${basePath}src/images/logo.png" width="30" height="30" class="d-inline-block align-top mr-2" alt="">
                 <a class="navbar-brand" href="#">FoodSafe</a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
