ca00e8ddaf2cc445cd0333676975dc262196db30
sudosupercat <230805509+sudosupercat@users.noreply.github.com>
2026-07-16 01:40:25 +0800

    fix

--- controller/admin/adminHomepage.controller.php
+++ controller/admin/adminHomepage.controller.php
@@ -9,8 +9,6 @@ if (!isset($_SESSION['userID']) || $_SESSION['role'] !== 'Admin') {
 $adminName = $_SESSION['firstName'] ?? 'Admin';
 
 require 'view/admin/homepage.php';
-=======
 
 require __DIR__ . '/../../view/admin/homepage.php';
->>>>>>> e24abdd8ac21bdd9cacaf2373233b4b3b6d85de5
 ?>
