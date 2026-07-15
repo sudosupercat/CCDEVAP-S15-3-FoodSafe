dbc9f0546fe5600f75d4ccf6b41da110f2bbb4ef
dreaPresto <andrea_prestoza@dlsu.edu.ph>
2026-07-16 05:03:38 +0800

    Fixed ComplaintController.php

--- controller/ComplaintController.php
+++ controller/ComplaintController.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../model/FoodBusiness.php';
+require_once __DIR__ . '/../model/FoodBusiness.model.php';
 require_once __DIR__ . '/../model/ComplaintModel.php';
 
 class ComplaintController {
@@ -16,8 +16,12 @@ class ComplaintController {
         return $this->foodBusinessModel->getAllIdName();
     }
 
-    public function submitComplaint($restoID, $complainantName, $complainantEmail, $details) {
-        return $this->complaintModel->createComplaint($restoID, $complainantName, $complainantEmail, $details);
+    public function getRequirementTypes() {
+        return $this->complaintModel->getRequirementTypes();
+    }
+
+    public function submitComplaint($restoID, $firstName, $lastName, $email, $contactNo, $requirementCode, $description) {
+        return $this->complaintModel->createComplaint($restoID, $firstName, $lastName, $email, $contactNo, $requirementCode, $description);
     }
 }
 ?>
