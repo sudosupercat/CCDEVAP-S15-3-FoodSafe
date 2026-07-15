ebced560907c0bf368f9ec05d474d0e0b104e68c
viknvm01 <vikturi01@gmail.com>
2026-07-16 00:58:26 +0800

    updated links

--- controller/inspector/inspectorReports.controller.php
+++ controller/inspector/inspectorReports.controller.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['userID'])) {
     exit();
 }
 
-require 'model/inspector.model.php';
+require __DIR__ . '../../model/inspector.model.php';
 
 
 // REPORTS -- UPDATE STATUS
@@ -30,6 +30,6 @@ if (isset($_GET['reportID'])) {
     $selectedReport = getReportByID($pdo, $_GET['reportID']);
 }
 
-require 'view/inspector/reports.php';
+require __DIR__ . '../../view/inspector/reports.php';
 
 ?>
