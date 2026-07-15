590b641f05db11d84a8845df23b2535c40f6ef68
miguel <mafmonterola@gmail.com>
2026-07-16 01:38:37 +0800

    edited homepage controller

--- controller/admin/adminHomepage.controller.php
+++ controller/admin/adminHomepage.controller.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['userID']) || $_SESSION['role'] !== 'Admin') {
-    header('Location: login');
+    header('Location: ../loginPage.controller.php');
     exit();
 }
 
