68b29ccb97d98ca19f1aea5d4c73fe9fe7357166
viknvm01 <vikturi01@gmail.com>
2026-07-15 23:30:26 +0800

    Update inspectorReports.controller.php

--- controller/inspector/inspectorReports.controller.php
+++ controller/inspector/inspectorReports.controller.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 if (!isset($_SESSION['userID'])) {
-    header("Location: controller/loginPage.controller.php");
+    header("Location: ../loginPage.controller.php");
     exit();
 }
 
@@ -20,7 +20,7 @@ if (isset($_POST['action']) && $_POST['action'] === 'update' && isset($_POST['id
     exit();
 }
 
-// REPORTS -- READ ; di pa tested yung session
+// REPORTS -- READ
 $userID = $_SESSION['userID'];
 $role = $_SESSION['role'];
 $reports = getReports($pdo, $userID, $role);
