0bd13d23d5fa010083fc546edf5a8150740323fb
dreaPresto <andrea_prestoza@dlsu.edu.ph>
2026-07-16 07:24:02 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/view/functions.php';
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($uri){
     case '/login':
@@ -40,6 +40,9 @@ switch ($uri){
         break;
     case '/inspectorHomepage':
         require 'controller/inspector/inspectorHomepage.controller.php';
+        break;
+    case '/restaurant-detail':
+        require 'controller/RestaurantDetailController.php';
         break;
     case '/business-directory':
         require 'controller/FoodBusiness.Controller.php';
