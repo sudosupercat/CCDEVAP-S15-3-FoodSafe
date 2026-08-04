d074951b5ed64e8a2b9f5b0c023dbde7d70c0a35
camora-sudo <clarisse.a.nazario@gmail.com>
2026-08-04 21:26:15 +0800

    Added a filter dropdown in inspector dashboard

--- controller/inspector/inspectorDashboard.controller.php
+++ controller/inspector/inspectorDashboard.controller.php
@@ -10,10 +10,13 @@ require 'config/db.php';
 require 'model/inspector.model.php';
 $userID = $_SESSION['userID'];
 
-$totalInspections = getTotalInspections($pdo, $userID);
-$pendingReports = getPendingReportsCount($pdo, $userID);
-$monthlyCounts = getInspectionsPerMonth($pdo, $userID);
-$gradeDistribution = getGradeDistribution($pdo, $userID);
+$currentYear = (int)date('Y');
+$selectedYear = isset($_GET['year']) ? (int)$_GET['year'] : $currentYear;
+$availableYears = getAvailableYears($pdo, $userID);
+$totalInspections = getTotalInspections($pdo, $userID, $selectedYear);
+$pendingReports = getPendingReportsCount($pdo, $userID, $selectedYear);
+$monthlyCounts = getInspectionsPerMonth($pdo, $userID, $selectedYear);
+$gradeDistribution = getGradeDistribution($pdo, $userID, $selectedYear);
 
 require 'view/inspector/dashboard.php';
 
