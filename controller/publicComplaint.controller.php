4120db03d844fa8bc94f9097b4971b6d80456c03
dreaPresto <andrea_prestoza@dlsu.edu.ph>
2026-08-06 01:05:56 +0800

    Update and rename userComplaint.controller.php to publicComplaint.controller.php

--- controller/publicComplaint.controller.php
+++ controller/publicComplaint.controller.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../model/authFoodBusiness.model.php';
-require_once __DIR__ . '/../model/userComplaint.model.php';
+require_once __DIR__ . '/../model/publicComplaint.model.php';
 
 class ComplaintController {
     private $foodBusinessModel;
