9c31b5ca2a028021d1d429c120e60ab80ee90a7e
dreaPresto <andrea_prestoza@dlsu.edu.ph>
2026-08-06 00:31:19 +0800

    Update and rename restaurantDetail.controller.php to adminRestaurantDetail.controller.php

--- controller/adminRestaurantDetail.controller.php
+++ controller/adminRestaurantDetail.controller.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../model/FoodBusiness.model.php';
+require_once __DIR__ . '/../model/adminFoodBusiness.model.php';
 
 class RestaurantDetailController {
     private $pdo;
